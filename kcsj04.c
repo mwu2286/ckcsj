b2fe4f08d00921396aad82803af85e1cf8791b92
mwu2286 <40415542+mwu2286@users.noreply.github.com>
2018-06-29 11:49:33 +0800

    Add files via upload

--- kcsj04.c
+++ kcsj04.c
@@ -5,7 +5,7 @@
 
     int n=0,i=0,j=0,num;
 	int gonghao[10];
-	double sum=0,sum1,sum2,tax;
+	double sum,sum1,sum2,tax;
 	void grsds(double sum);
 
 	struct zggz{
@@ -18,16 +18,15 @@
 		float ying;//应发工资
 		float ge;//个人所得税
 		float shi;//实发工资
-}zggza[100],zggzb[100],*p,*q;
+}zggza[200],zggzb[200],*p,*q;
 
 
 void read()
 {	
-	int n=0;
+	
 	int ch;
     FILE *fp;
-	p=zggza;
-if((fp=fopen("gx.dat","r"))==NULL)	//文件打开失败
+if((fp=fopen("gx.dat","rb"))==NULL)	//文件打开失败
 {
 	printf("文件打开失败！\n");
 	exit(-1);
@@ -35,15 +34,12 @@ if((fp=fopen("gx.dat","r"))==NULL)	//文件打开失败
 }
 else
     printf("文件打开成功！\n");
-	ch=zggza[n].num;
-    do
-	{	
-		zggza[n];
-	    fread(p,sizeof(struct zggz),n,fp);
-		printf("%d%s%f%f%f%f%f",zggza[n].num,zggza[n].name,zggza[n].gang,zggza[n].xin,zggza[n].zhi,zggza[n].ji);
-		n++;
-	}while(ch="\0");
-	fclose(fp);
+	    for(n=0;n<1;n++)
+		{
+		fscanf(fp,"%s%s%f%f%f%f%f%f%f\n",zggza[n].num,zggza[n].name,zggza[n].gang,zggza[n].xin,zggza[n].zhi,zggza[n].ji,zggza[n].ying,zggza[n].ge,zggza[n].shi);
+		printf("zggza[n].num=%s,zggza[n].name=%s,zggza[n].gang=%f,zggza[n].xin=%f,zggza[n].zhi=%f,zggza[n].ji=%f,zggza[n].ying=%f,zggza[n].ge=%f,zggza[n].shi=%f\n",zggza[n].num,zggza[n].name,zggza[n].gang,zggza[n].xin,zggza[n].zhi,zggza[n].ji,zggza[n].ying,zggza[n].ge,zggza[n].shi);
+		}
+		fclose(fp);
 }
 
 void write()    //保存
@@ -61,21 +57,18 @@ void write()    //保存
 
 void find()    //查询
 {	
-	
 	int ch;
-	int i=0;
 	FILE *fp;
 	p=zggza;
-	ch=zggza[i].num;
 	printf("请输入工号！\n");
 	scanf("%d","&gonggao");
 	do
-	{    
-		fread(p,sizeof(struct zggz),i,fp);
-	    zggza[i].num;
-		i++;
-	}while(gonghao[10]=ch);
-printf("%d","zggza[i-1]");
+	{   n=0;
+		ch=zggza[i].num;
+		fscanf(fp,"%s%s%f%f%f%f%f%f%f\n",zggza[n].num,zggza[n].name,zggza[n].gang,zggza[n].xin,zggza[n].zhi,zggza[n].ji,zggza[n].ying,zggza[n].ge,zggza[n].shi);
+		n++;
+	}while(ch=gonghao[10]);
+printf("%d""zggza[n-1]");
 }
 
 void list()	//浏览
@@ -93,7 +86,7 @@ void modift()	//修改
     printf("请输入职工基本信息");
 	scanf("%d%s%f%f%f%f",zggza[i-1].num,zggza[i-1].name,zggza[i-1].gang,zggza[i-1].xin,
 		zggza[i-1].zhi,zggza[i-1].ji);
-	sum=zggza->gang+zggza->xin+zggza->zhi+zggza->ji;
+	sum=zggza[i-1].gang+zggza[i-1].xin+zggza[i-1].zhi+zggza[i-1].ji;
 	grsds(sum);
 	printf("%d%s%f%f%f%f%f%f%f%f"
 		,zggza->num,zggza->name,zggza->gang,zggza->xin,zggza->zhi,zggza->ji,sum,tax,sum2);
@@ -122,16 +115,16 @@ void add()	//添加
 {
 	n++;
 	printf("请输入职工基本信息");
-	scanf("%d%s%f%f%f%f",zggza->num,zggza->name,zggza->gang,zggza->xin,zggza->zhi,zggza->ji);
-	sum=zggza->gang+zggza->xin+zggza->zhi+zggza->ji;
+	scanf("%d%s%f%f%f%f",zggza[n].num,zggza[n].name,zggza[n].gang,zggza[n].xin,zggza[n].zhi,zggza[n].ji);
+	sum=sum+zggza[n].gang+zggza[n].xin+zggza[n].zhi+zggza[n].ji;
 	grsds(sum);
 	printf("%d%s%f%f%f%f%f%f%f%f"
-		,zggza->num,zggza->name,zggza->gang,zggza->xin,zggza->zhi,zggza->ji,sum,tax,sum2);
+		,zggza[n].num,zggza[n].name,zggza[n].gang,zggza[n].xin,zggza[n].zhi,zggza[n].ji,sum,tax,sum2);
 }
 
 void grsds(double sum)//计算个人所得税
 {
-	sum=zggza->gang+zggza->xin+zggza->zhi+zggza->ji;
+	sum=sum+zggza[n].gang+zggza[n].xin+zggza[n].zhi+zggza[n].ji;
 	sum1=sum-3500;//给人所得税起征点3500，sum1为应缴个税部分工资。
 	if(sum>100000)
 	{
